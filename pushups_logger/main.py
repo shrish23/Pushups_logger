f1c430313cf274264a1145ff504460aa5f024d4f
shrish <shrishsharma@outlook.com>
2021-03-04 16:33:46 +0530

    Complted first flask project

---
+++ pushups_logger/main.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, render_template,url_for, request, redirect,flash
+from flask_login import login_required, current_user
+from .models import Workout, User
+from . import db
+
+main = Blueprint('main',__name__)
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+@main.route('/new')
+@login_required
+def new_workout():
+    return render_template('create_workout.html')
+
+
+@main.route('/new', methods=['POST'])
+@login_required
+def new_workout_post():
+    pushups = request.form.get('pushups')
+    comment = request.form.get('comment')
+
+    workout = Workout(pushups=pushups,comment=comment,author=current_user)
+    db.session.add(workout)
+    db.session.commit()
+    flash("Your Workout has been Added")
+
+    return redirect(url_for('main.user_workout'))
+
+@main.route('/all')
+@login_required
+def user_workout():
+    page = request.args.get('page',1, type=int)
+    user = User.query.filter_by(email=current_user.email).first_or_404()
+    workouts = Workout.query.filter_by(author=user).paginate(page=page,per_page=3)
+    return render_template('all_workouts.html', workouts=workouts, user=user)
+
+@main.route('/workout/<int:workout_id>/update',methods=['POST','GET'])
+@login_required
+def update_workout(workout_id):
+    workout = Workout.query.get_or_404(workout_id)
+    if(request.method == 'POST'):
+        workout.pushups = request.form['pushups']
+        workout.comment = request.form['comment']
+        db.session.commit()
+        flash("Your workout has been updated")
+        return redirect(url_for('main.user_workout'))
+
+    return render_template('update_workout.html',workout=workout)
+
+@main.route('/workout/<int:workout_id>/delete',methods=['POST','GET'])
+@login_required
+def delete_workout(workout_id):
+    workout = Workout.query.get_or_404(workout_id)
+    db.session.delete(workout)
+    db.session.commit()
+    flash("Your post has been deleted")
+    return redirect(url_for('main.user_workout'))
